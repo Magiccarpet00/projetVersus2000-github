dcb7748cbbd1cc6af541c1d74dfe1c51c34cf5df
Félix Tellitocci <73948501+Magiccarpet00@users.noreply.github.com>
2021-03-22 16:46:24 +0100

    detection joueur
    methode dans gamemanager pour detecteur le joueur dans une room

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public LevelGenerator levelGenerator;
+    public GameObject startRoom;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GameObject room = whereIsPlayer();
+            Debug.Log(room);
+        }
+        
+    }
+
+    public GameObject whereIsPlayer()
+    {
+        for (int i = 0; i < levelGenerator.roomsInDongeon.Capacity; i++)
+        {
+            if(levelGenerator.roomsInDongeon[i].GetComponent<Room>().playerOnThisRoom == true)
+            {
+                return levelGenerator.roomsInDongeon[i];
+            }      
+        }
+        return startRoom;
+    }
+
+
+}
