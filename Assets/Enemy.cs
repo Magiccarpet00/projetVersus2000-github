b6a0aadfbf1d0c43c72a6fe7cf76d5a0602ef5fe
Félix Tellitocci <73948501+Magiccarpet00@users.noreply.github.com>
2021-03-23 14:59:18 +0100

    Premiere enemy
    j'ai pas fait un enemy generique pour l'instant

---
+++ Assets/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Varrible Statistique
+    public float maxHealth;
+    public float currentHealth;
+    public float damage;
+    public bool dead;
+
+    // Varriable patrol
+    public float speed;
+    public Transform[] wayPoints;
+    private Transform target;
+    private int destinationPoint;
+    public bool activated;
+
+    // Varrible bubble 
+    public bool haveBubble;
+
+    public void Start()
+    {        
+        target = wayPoints[0];
+    }
+
+    private void Update()
+    {
+        if (activated && !dead)
+        {
+            Patrol();
+        }        
+    }
+
+    public void Patrol()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        //Si l'ennemi est quasiment arriver à destination
+        if (Vector3.Distance(transform.position, target.position) < Constants.DESTINATION_PROCHE)
+        {
+            destinationPoint = (destinationPoint + 1) % wayPoints.Length;
+            target = wayPoints[destinationPoint];
+        }
+    }
+
+}
--- Assets/Script/Constants.cs
+++ Assets/Script/Constants.cs
@@ -4,5 +4,6 @@ using UnityEditor;
 public static class Constants
 {
     public const float CIRCLE_RADIUS = .2f;
+    public const float DESTINATION_PROCHE = .01f;
     public const float OFFSET = 8f;   
 }
