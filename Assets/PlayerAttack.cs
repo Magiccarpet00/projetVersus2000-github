c599fb5d96afbc19cb4bf2cf2c384349c3b05c52
Benoît Cante <cantebenoit@hotmail.com>
2021-04-04 18:22:10 +0200

    Offset + angle adulte version
    ct dur

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -5,18 +5,28 @@ using static PlayerMovement;
 
 public class PlayerAttack : MonoBehaviour
 {
-    Dictionary<InputBufferDirection, Vector3> directionOffSet_And_Rotation = new Dictionary<InputBufferDirection, Vector3>();
-    
+    Dictionary<InputBufferDirection, InfoAttack> directionOffSet_And_Rotation = new Dictionary<InputBufferDirection, InfoAttack>();
+    public struct InfoAttack
+    {
+        public InfoAttack(Vector2 offsetAttack, float rotationAttack, float delayAttack)
+        {
+            OffsetAttack = offsetAttack;
+            RotationAttack = Quaternion.Euler(0f, 0f, rotationAttack);
+            DelayAttack = delayAttack;
+        }
+
+        public Vector2 OffsetAttack { get; set; }
+        public Quaternion RotationAttack { get; set; }
+        public float DelayAttack { get; set; }
+    }
+
     public void Awake()
     {
         // on regarde dans quelle dirrection est l'input buffer
-
-
-        directionOffSet_And_Rotation[InputBufferDirection.UP] = new Vector3(0f,Constants.OFFSET_ATTACK,90f);
-        directionOffSet_And_Rotation[InputBufferDirection.LEFT] = new Vector3(-Constants.OFFSET_ATTACK, 0f, 180f); 
-        directionOffSet_And_Rotation[InputBufferDirection.DOWN] = new Vector3(0f, -Constants.OFFSET_ATTACK, 270f);
-        directionOffSet_And_Rotation[InputBufferDirection.RIGHT] = new Vector3(Constants.OFFSET_ATTACK, 0f, 0f);
-
+        directionOffSet_And_Rotation[InputBufferDirection.UP] = new InfoAttack(new Vector2(0f, Constants.OFFSET_ATTACK), 90f,0.5f);
+        directionOffSet_And_Rotation[InputBufferDirection.LEFT] = new InfoAttack(new Vector2(-Constants.OFFSET_ATTACK,0f), 180f, 0.5f);
+        directionOffSet_And_Rotation[InputBufferDirection.DOWN] = new InfoAttack(new Vector2(0f, -Constants.OFFSET_ATTACK), 270f, 0.5f);
+        directionOffSet_And_Rotation[InputBufferDirection.RIGHT] = new InfoAttack(new Vector2(Constants.OFFSET_ATTACK, 0f), 0f, 0.5f);
 
     }
     public GameObject epeePrefab;
@@ -38,17 +48,19 @@ public class PlayerAttack : MonoBehaviour
     }
 
     public IEnumerator Attack()
-    {                
+    {
+
+        InfoAttack infoAttack = directionOffSet_And_Rotation[playerMovement.InputBuffer];
+        GameObject epee = Instantiate(epeePrefab, transform.position + transform.TransformDirection(infoAttack.OffsetAttack), infoAttack.RotationAttack);
 
 
-        GameObject epee = Instantiate(epeePrefab,
-                                       new Vector2(transform.position.x + directionOffSet_And_Rotation[playerMovement.InputBuffer].x, transform.position.y + directionOffSet_And_Rotation[playerMovement.InputBuffer].y),
-                                       Quaternion.Euler(0f, 0f, directionOffSet_And_Rotation[playerMovement.InputBuffer].z));
-
+        //GameObject epee = Instantiate(epeePrefab,
+        //                               new Vector2(transform.position.x + directionOffSet_And_Rotation[playerMovement.InputBuffer].x, transform.position.y + directionOffSet_And_Rotation[playerMovement.InputBuffer].y),
+        //                               Quaternion.Euler(0f, 0f, directionOffSet_And_Rotation[playerMovement.InputBuffer].z));
 
         epee.GetComponent<Animator>().SetTrigger("Attack");
-
-        yield return new WaitForSeconds(0.4f);
+        yield return new WaitForSeconds(infoAttack.DelayAttack);
+        
         Destroy(epee);
         playerMovement.UnStopMovement();
     }
