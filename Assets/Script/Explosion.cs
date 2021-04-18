a8720d2fd6f089e3e3caeafd900b3508a3f70395
Benoît Cante <cantebenoit@hotmail.com>
2021-04-18 21:16:00 +0200

    avant de faire des bêtises de déplacement de component
    commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit commit

--- Assets/Script/Explosion.cs
+++ Assets/Script/Explosion.cs
@@ -12,6 +12,18 @@ public class Explosion : MonoBehaviour
 
     public IEnumerator ActivationHitbox()
     {
+        Animator animator = this.GetComponentInChildren<Animator>();
+        float animLength;
+        RuntimeAnimatorController ac = animator.runtimeAnimatorController;    //Get Animator controller
+
+        foreach (var clip in ac.animationClips)
+        {
+            if (clip.name == "explosion") // valeur en dur ;)
+            {
+                animLength = clip.length;
+                // si on étiat propre on quitterait la boucle à ce moment
+            }
+        }
         /*
          * [CODE REVIEW] Comment retrouver la durée d'exécution d'une animation
          * Félix est incapable de trouver ça tout seul du coup je vais l'aider "j'ai cherché et j'ai pas trouvé jte jure" (mon oeil)
@@ -19,7 +31,12 @@ public class Explosion : MonoBehaviour
 
         yield return new WaitForSeconds(0.15f); // have you heard about Constants? 
         hitbox.enabled = true; // pour pas buter instant les autres mobs
-        yield return new WaitForSeconds(0.35f);
+        yield return new WaitForSeconds(0.35f); // 350ms
         Destroy(this.gameObject);
+    }
+
+    public void called()
+    {
+
     }
 }
