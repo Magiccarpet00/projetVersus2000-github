82221f9dd4e2bb1b4ca51363719ec8dea5a76c4d
Félix Tellitocci <73948501+Magiccarpet00@users.noreply.github.com>
2021-05-05 15:15:28 +0200

    caisse qui peuvent être casser

---
+++ Assets/Script/DestructibleObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+    public SpriteRenderer[] sprite;
+    public Animator animator;
+    public BoxCollider2D boxCollider;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Epée") || collision.CompareTag("Explosion"))
+        {
+            for (int i = 0; i < sprite.Length; i++) // Desactiver les sprites de la caisse statique
+            {
+                sprite[i].enabled = false;
+            }
+
+            animator.SetTrigger("Destroy");
+            boxCollider.enabled = false;
+
+        }
+    }
+}
