8d04e6640572644bf590b91258c5c8b21a5c6f68
Félix Tellitocci <73948501+Magiccarpet00@users.noreply.github.com>
2021-06-08 15:30:13 +0200

    Player selected

---
+++ Assets/CharacterSelectButton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectButton : MonoBehaviour
+{
+    public Character character;
+    public Image image;
+
+    public Image blueImage;
+    public Image redImage;
+    public Image greenImage;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+
+        if(character == Character.BLUE)
+        {
+            image.sprite = blueImage.sprite;
+        }
+        else if(character == Character.RED)
+        {
+            image.sprite = redImage.sprite;
+        }
+        else if (character == Character.GREEN)
+        {
+            image.sprite = greenImage.sprite;
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
